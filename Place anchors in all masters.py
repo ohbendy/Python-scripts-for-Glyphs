92489e36d3b0c81e308f470c7f12de9a75fd3ae8
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2015-10-23 01:59:05 +0200

    Code Cleanup
    Avoid code reduplication, iterative rather than (less efficient)
    procedural approach.

--- Place anchors in all masters.py
+++ Place anchors in all masters.py
@@ -4,106 +4,39 @@
 
 import GlyphsApp
 
-thisFont = Glyphs.font
+anchorDict = {
+	"Light":   (-300, 600),
+	"Regular": (-315, 610),
+	"Bold":    (-320, 620),
+	"Light Condensed": (-250, 600),
+	"Condensed":       (-255, 610),
+	"Bold Condensed":  (-260, 620)
+}
 
 # This script will add _top attachment anchors to selected mark glyphs at the ( x,y ) coordinates in each master named.
 # Adapt master names, anchor names and anchor positions as required.
 # Add other anchors under each master, no problem.
 
-for glyph in [l.parent for l in thisFont.selectedLayers]:
+def addAnchorToLayer( thisLayer, anchorname="_top", anchorposition=(300,600) ):
+	if thisLayer:
+		newAnchor = GSAnchor.alloc().init()
+		newAnchor.name = anchorname
+		thisLayer.addAnchor_( newAnchor )
+		newPosition = NSPoint( anchorposition[0], anchorposition[1])
+		newAnchor.setPosition_( newPosition )
 
-			# LIGHT master first:
-	for master in thisFont.masters:
-		if master.name == 'Light':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
+thisFont = Glyphs.font
+allSelectedGlyphs = [l.parent for l in thisFont.selectedLayers]
+allMasterNames = anchorDict.keys() # master names listed in anchorDict
 
-			# set its position:
-	position = ( -300, 600 )
-	_topAnchor.setPosition_( position )
-			
-			# REGULAR master next:
-	for master in thisFont.masters:
-		if master.name == 'Regular':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
-
-			# set its position:
-	position = ( -315, 610 )
-	_topAnchor.setPosition_( position )
-	
-			# BOLD master next:
-	for master in thisFont.masters:
-		if master.name == 'Bold':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
-
-			# set its position:
-	position = ( -320, 620 )
-	_topAnchor.setPosition_( position )
-	
-			# LIGHTCONDENSED master next:
-	for master in thisFont.masters:
-		if master.name == 'Light Condensed':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
-
-			# set its position:
-	position = ( -250, 600 )
-	_topAnchor.setPosition_( position )
-	
-				# CONDENSED master next:
-	for master in thisFont.masters:
-		if master.name == 'Condensed':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
-
-			# set its position:
-	position = ( -255, 610 )
-	_topAnchor.setPosition_( position )
-	
-			# BOLDCONDENSED master next:
-	for master in thisFont.masters:
-		if master.name == 'Bold Condensed':
-				id = master.id
-				break
-	layer = glyph.layers[id]
-			
-			# add _top anchor:
-	_topAnchor = GSAnchor.alloc().init()
-	_topAnchor.name = "_top"
-	layer.addAnchor_(_topAnchor)
-
-			# set its position:
-	position = ( -260, 620 )
-	_topAnchor.setPosition_( position )
+# iterate through all layers of all selected glyphs:
+for thisGlyph in allSelectedGlyphs:
+	for thisLayer in thisGlyph.layers:
+		# determine the master to which the layer belongs:
+		masterID = thisLayer.associatedMasterId
+		thisMasterName = thisFont.masters[masterID].name
+		# if it is listed in anchorDict, add the anchor at the given position:
+		if thisMasterName in allMasterNames:
+			thisPositionXY = anchorDict[thisMasterName]
+			thisPosition = NSPoint( thisPositionXY[0], thisPositionXY[1] )
+			addAnchorToLayer( thisLayer, anchorposition=thisPosition )
