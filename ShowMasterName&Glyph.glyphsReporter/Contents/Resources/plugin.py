d40c8508d3700bf637ef221075122c9422e98f68
Ben Mitchell <ohbendy@hotmail.com>
2016-02-28 13:52:33 +0000

    Adapted Georg's improvements to keep text above the reporter for incompatible masters, tweaked colours, adjusted text size.

--- ShowMasterName&Glyph.glyphsReporter/Contents/Resources/plugin.py
+++ ShowMasterName&Glyph.glyphsReporter/Contents/Resources/plugin.py
@@ -19,38 +19,59 @@ class ShowMasterNameAndGlyph(ReporterPlugin):
 	def settings(self):
 		self.menuName = Glyphs.localize({'en': u'Master Name & Glyph'})
 		
-	def foreground(self, layer):
-		currentScale = self.getScale()
-		textSize = 10 + currentScale * 4
-		masterName = layer.associatedFontMaster().name
-		self.drawTextAtPoint(
-			masterName, 
-			NSPoint(self.textX, self.textY + ((16 + currentScale * 6) / currentScale)),
-			fontSize=textSize,
-			align = "bottomcenter",
-			fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_(0.1 , 0.7 , 0.5, 0.5)
-		)
+	def foregroundFlat(self):
+		scale = self.getScale()
+		HandleSize = Glyphs.defaults["GSHandleSize"]
+		if HandleSize == 0:
+			textSize = 9+scale*6;
+		elif HandleSize == 2:
+			textSize = 13+scale*6;
+		else:
+			textSize = 11+scale*6;
+		if scale < 1:
+			textSize *= pow(scale, 0.1)
+		
+		layer = self.activeLayer()
+		position = self.activePosition()
+		
+		master = layer.associatedFontMaster()
+		masterName = master.name
 		
 		thisGlyph = layer.parent
 		if thisGlyph.unicode:
-			textToDisplay = thisGlyph.string
+			glyph = thisGlyph.string
 		else:
-			textToDisplay = thisGlyph.name
+			glyph = thisGlyph.name
+		textToDisplay = masterName + " : " + glyph
 		
-		self.drawTextAtPoint(
-			textToDisplay,
-			NSPoint(self.textX, self.textY),
-			fontSize=textSize * 1.3,
-			align = "bottomcenter",
-			fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_(0.1 , 0.8 , 0.7, 0.8)
-		)
-		
-	def background(self, layer):
 		if layer.width < 1:
-			self.textX = (layer.bounds.origin.x + (0.5 * layer.bounds.size.width))
+			textX = layer.bounds.origin.x + (0.5 * layer.bounds.size.width)
 		else:
-			self.textX = (layer.width / 2)
-		self.textY = max((layer.bounds.origin.y + layer.bounds.size.height), layer.glyphMetrics()[1])
+			textX = layer.width / 2
+		textY = max((layer.bounds.origin.y + layer.bounds.size.height), master.ascender)
+		
+		textX = position.x + (textX * scale)
+		textY = position.y + (textY * scale) + 35
+		
+		self.drawBadge(
+			textToDisplay,
+			NSPoint(textX, textY),
+			fontSize = textSize,
+			align = GSBottomCenter,
+			backgroundColor = NSColor.colorWithCalibratedRed_green_blue_alpha_(0.55 , 0.51 , 0.48, 0.2),
+			visibleRect = True
+		)
+	
+	def drawBadge(self, text, drawPoint, fontSize = 12.0, fontColor=NSColor.colorWithCalibratedRed_green_blue_alpha_(0.22 , 0.82 , 0.72, 0.6), backgroundColor = None, align = GSBottomLeft, visibleRect = False, active = False):
+		glyphEditView = self.controller.graphicView()
+		if visibleRect:
+			visibleRect = glyphEditView.visibleRect()
+		else:
+			visibleRect = NSMakeRect(-10000000, -10000000, 20000000, 20000000)
+		
+		glyphEditView.drawBadge_size_color_backgroundColor_atPoint_alignment_visibleInRect_active_(text, fontSize, fontColor, backgroundColor, drawPoint, align, visibleRect, active)
+	
+	def ___background(self, layer):
 		
 		currentScale = self.getScale()
 		rectSize = (20 + currentScale * 4) / currentScale
